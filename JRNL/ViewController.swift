069e02b7ea9a5fec255f95ea9f0d2ebf7733de40
M <mm@Ms-iMac.local>
2017-12-21 16:36:52 +0100

    a

--- JRNL/ViewController.swift
+++ JRNL/ViewController.swift
@@ -8,31 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UICollectionViewDelegate {
 
     //MARK: Properties
     
     @IBOutlet weak var TestLabel: UILabel!
     @IBOutlet weak var testTextField: UITextField!
+    @IBOutlet weak var testButton: UIButton!
+    @IBOutlet weak var colViewCalender: UICollectionView!
+    @IBOutlet weak var colViewCalCell: UICollectionViewCell!
     
     //MARK: Actions
-    
-    @IBOutlet weak var testButton: UIButton!
-    
+
     @IBAction func testAction(_ sender: UIButton) {
         testTextField.text = "blub"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        testTextField.delegate = self
+        colViewCalender.delegate = self
+        
+        var bla: UICollectionViewDataSource
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: UITextFieldDelegatetetet
+    
+    func textFieldShouldReturn(_ textFieldd: UITextField) -> Bool {
+        textFieldd.resignFirstResponder()
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        TestLabel.text = textField.text
+    }
 
-
+    
 }
 
